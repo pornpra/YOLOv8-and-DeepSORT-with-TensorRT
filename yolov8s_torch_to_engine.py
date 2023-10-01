00295b09285eaf1db8263adbacdb2446c7ac2c0d
pornpra <59501173+pornpra@users.noreply.github.com>
2023-10-01 13:23:15 +0700

    Add files via upload

---
+++ yolov8s_torch_to_engine.py
@@ -0,0 +1,26 @@
+import torch
+import cv2
+import onnxruntime as onnxrt
+from ultralytics import YOLO
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+map_location =  torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# load a custom trained
+model_weight = './yolov8s.pt'
+model = YOLO(model_weight)
+
+# Export the model
+model.export(format='engine', device=device, half=True)
+
+## Don't forget to check and rename converted model tp yolov8s_fp16.engine ##
+
+# Inference with tensorrt
+input_img = './test_image.jpg'
+img = cv2.imread(input_img)
+pred = YOLO('./yolov8s_fp16.engine', task='detect').predict(img, device=device)
+res_plotted = pred[0].plot()
+cv2.imshow('res_plotted', res_plotted)
+pred[0].boxes.data
+
+
